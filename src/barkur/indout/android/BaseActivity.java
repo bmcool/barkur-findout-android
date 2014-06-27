482099d8f7ac1662fe68fa5a506661558a1d9b0a
HeathHuang <heathhuang0701@gmail.com>
2014-06-28 03:13:21 +0800

    [add] create project

---
+++ src/barkur/indout/android/BaseActivity.java
@@ -0,0 +1,14 @@
+package barkur.indout.android;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+	protected Context mContext;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
